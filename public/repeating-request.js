42d77d4df9dd1747fe419101f46025afd30b337b
Ian Peter Greenburg <iangreenburg@yahoo.com>
2017-03-04 17:04:14 -0800

    able to repeatedly request from api successfully

--- public/repeating-request.js
+++ public/repeating-request.js
@@ -2,21 +2,19 @@ var http = require('http');
 require('dotenv').config()
 const TRANSLINK_API_KEY = process.env.TRANSLINK_API_KEY
 
-var options = {
-  host: 'www.api.translink.ca',
-  path: '/rttiapi/v1/buses?apikey=' + TRANSLINK_API_KEY
-};
+var request=require("request");
 
-function request() {
-  http.request(options, function(res){
-    res.on('data', function(chunk){
-       console.log(chunk);
-    });
-  }).on("error", function(e){
-    console.log("Got error: " + e.message);
-  });
+var requost = function(){
+	request.get('http://api.translink.ca/rttiapi/v1/buses?apikey=' + TRANSLINK_API_KEY,
+		function(error,response,body){
+		       if(error){
+		             console.log('error:', error);
+		       }else{
+		             console.log(response);
+		     }
+		})
 }
 
 module.exports = function(){
-	setInterval(request, 2000);
+	setInterval(requost, 2000);
 }
